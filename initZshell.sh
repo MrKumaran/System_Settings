77387be36e89499dd263b15fa753e69088804123
Kumaran S <mrkumarancse@gmail.com>
2026-07-05 01:01:24 +0530

    Update curl commands to use silent mode

--- initZshell.sh
+++ initZshell.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-curl https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.vimrc >> ~/.vimrc
-curl https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.zshrc >> ~/.zshrc
+curl -s https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.vimrc >> ~/.vimrc
+curl -s https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.zshrc >> ~/.zshrc
