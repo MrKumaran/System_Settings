221e46672642b6df443a30c55f2f23f6d7a38ed6
Kumaran S <mrkumarancse@gmail.com>
2026-07-05 00:44:50 +0530

    Init zshell script

---
+++ initZshell.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+curl https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.vimrc >> ~/.vimrc
+curl https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.zshrc >> ~/.zshrc
