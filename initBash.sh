c39899b5e43a4bf5ffc84b59ce12738eb73ee127
Kumaran S <mrkumarancse@gmail.com>
2026-07-05 01:25:08 +0530

    Bash init script

---
+++ initBash.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -s https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.vimrc >> ~/.vimrc
+curl -s https://raw.githubusercontent.com/MrKumaran/System_Settings/main/.bashrc >> ~/.bashrc
